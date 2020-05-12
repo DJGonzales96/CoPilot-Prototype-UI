37b46e104eebc7388ddccea826658014a5c5d671
Devin Gonzales <devin.j.gonzales@gmail.com>
2020-05-12 15:11:22 -0700

    init signUp page

---
+++ signUp/signUp.jsx
@@ -0,0 +1,60 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./signUp.css";
+
+class SignUp extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="2598fd04-aa02-4b76-9ca4-9e6f052fc8f6" className="signUp">        <div data-layer="fefcca74-125c-401c-8cd5-88108f38be08" className="continue">            <div data-layer="455c5226-618e-4707-987b-9b2b5f6fcf95" className="rectangle119"></div>
+            <div data-layer="d54a5944-8207-4522-b029-a2f0d2836c59" className="createAccount">CREATE ACCOUNT</div>
+</div>
+        <div data-layer="066c9984-5cd1-4bb7-b8ce-a23898e72336" className="accountSignup">            <div data-layer="69b6a413-da83-4ac7-9321-7043cd94e8df" className="rectangle84"></div>
+            <div data-layer="12d7df03-7f36-4c38-8b56-e72894e92189" className="inputa64d2c29">                <div data-layer="b9ba8589-92b3-4c4a-b7df-9cd148056258" className="input8b07a81c">                    <div data-layer="c37bc24d-7704-4703-9b45-c4f7e29708ec" className="rectangle855c5a4e1a"></div>
+                    <div data-layer="f05f6aa7-f150-4c1b-a7ca-d4addeee11c6" className="fullName">Full Name</div>
+</div>
+                <div data-layer="d2358cf2-4cf9-4b4b-9f24-950f48a452ec" className="input83be31a7">                    <div data-layer="5e08969b-f872-4eb4-a9a5-75d428b5df3b" className="rectangle852b379d05"></div>
+                    <div data-layer="30563e68-ae7b-4729-a50a-7112851b2222" className="email">Email</div>
+</div>
+                <div data-layer="06aeb1e1-0b12-4100-8611-a944c1bc7aa9" className="inputde7be36d">                    <div data-layer="b34b33c3-007e-4f84-9369-199205cf77ad" className="rectangle859c0d7218"></div>
+                    <div data-layer="bb60bda8-4d65-4fbf-b34d-f96c6065aa3b" className="password">Password</div>
+</div>
+                <div data-layer="edee848f-3846-4e1d-ac1e-feb7b086b6aa" className="input">                    <div data-layer="545f0dd6-0445-4ec4-80a0-73ef635ad84e" className="rectangle85"></div>
+                    <div data-layer="f70e37e8-1308-46a5-a570-0169a4e1a2eb" className="confirmPassword">Confirm Password</div>
+</div>
+</div>
+            <div data-layer="3ee51d43-b05a-4b0c-801a-42c29a4d33bd" className="createAnAccount">Create an Account</div>
+            <div data-layer="b28e9ac8-ea46-41fd-b134-a44865a5559f" className="byCreatingAnAccountYouAgreeToOurtermsOfServiceAndPrivacyPolicy">By creating an account you agree to our<br />Terms of Service and Privacy Policy</div>
+</div>
+        <div data-layer="607695af-b117-4822-927a-baf398ec4aec" className="navigationBar">            <svg data-layer="60d80c6a-6328-496c-b1ac-8b8667113013" preserveAspectRatio="none" viewBox="4907.00048828125 1088.9998779296875 375 67.999755859375" className="mergedBar"><path d="M 4907.00048828125 1156.999633789063 L 4907.00048828125 1108.999389648438 L 5282.00048828125 1108.999389648438 L 5282.00048828125 1156.999633789063 L 4907.00048828125 1156.999633789063 Z M 4907.00048828125 1108.999389648438 L 4907.00048828125 1088.999877929688 L 5282.00048828125 1088.999877929688 L 5282.00048828125 1108.999389648438 L 4907.00048828125 1108.999389648438 Z"  /></svg>
+            <div data-layer="f5fa6c01-601c-4522-9846-426529ae0b25" className="register">REGISTER</div>
+            <div data-layer="c2d9f062-6c3f-4e56-821b-d80d9e40ba45" className="backwardArrow">                <svg data-layer="dcf7f426-fb77-48b0-98d0-de6a57041ea3" preserveAspectRatio="none" viewBox="0 0 16 16" className="path10"><path d="M 8 0 L 6.545454978942871 1.454545497894287 L 12.05194854736328 6.961039066314697 L 0 6.961039066314697 L 0 9.038961410522461 L 12.05194854736328 9.038961410522461 L 6.545454978942871 14.54545497894287 L 8 16 L 16 8 L 8 0 Z"  /></svg>
+</div>
+</div>
+</div>
+
+    );
+  }
+}
+
+SignUp.propTypes = {
+
+}
+
+SignUp.defaultProps = {
+
+}
+
+
+export default SignUp;
+          
