1ebb88b570bc699b9770b08e6c3be3424c9aba00
Devin Gonzales <devin.j.gonzales@gmail.com>
2020-05-12 15:06:29 -0700

    init logIn page

---
+++ logIn/logIn.jsx
@@ -0,0 +1,55 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./logIn.css";
+
+class LogIn extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="f5dc4035-9f5a-47cc-96c2-acdbdae4cf80" className="logIn">        <div data-layer="b47096ea-214a-49b1-b7d6-b491c966f47c" className="login">            <div data-layer="bd7b1912-3165-4c66-a6d1-f3ac2067c97a" className="rectangle133"></div>
+            <div data-layer="6906449f-9324-48b8-95e4-214fc7835236" className="signUp">SIGN UP</div>
+</div>
+        <div data-layer="6fe80171-5c09-4ca8-8125-ec469e28cd53" className="continuef5b12268">            <div data-layer="70263d06-d23f-4026-981a-91af7726e9b6" className="rectangle132"></div>
+            <div data-layer="e127a7e2-fa95-432c-a99f-d35682ba41a4" className="continue">CONTINUE</div>
+</div>
+        <div data-layer="7764d858-9cce-4516-aa4b-a0a6513ac01c" className="header">            <div data-layer="79c4b016-33c8-4d7a-91a1-c8a942467b0e" className="rectangle134"></div>
+            <div data-layer="86af5b04-e835-4648-923a-b12dc2c4af6a" className="welcome">WELCOME</div>
+            <div data-layer="2538f541-701a-44f7-b654-de6c0624a574" className="logIn46800348">LOG IN</div>
+</div>
+        <div data-layer="271de37e-50a4-4c97-89d7-0b7f98f165a7" className="inputsa9ff2d8a">            <div data-layer="e540786a-240b-4813-9d59-ad1d9fad7e57" className="inputs4b995c07">                <div data-layer="725b9018-800a-4e09-8197-95dbb040f69f" className="rectangle1230a6a2c44"></div>
+                <div data-layer="4ece24c9-5090-4755-8c54-e605e1d99fd3" className="usernameHere">Username Here</div>
+</div>
+            <div data-layer="939c6e0e-d661-40d1-b9a0-767f231cdf33" className="inputs">                <div data-layer="d8ed21c1-f4ec-46b7-84ae-d809549f5996" className="rectangle123"></div>
+                <div data-layer="be7eef54-35f0-4b6a-8b9a-b7fd135eb338" className="x4e967966">●●●●●●●</div>
+</div>
+</div>
+        <div data-layer="377ab7bd-91a4-4670-b130-21189046e0b5" className="divider"></div>
+        <div data-layer="c8b5b88c-96bc-4415-803b-21ea91ff0f6a" className="username">Username</div>
+        <div data-layer="467cceb9-d426-4b6b-91ad-ae1aa07ce780" className="password">Password</div>
+</div>
+
+    );
+  }
+}
+
+LogIn.propTypes = {
+
+}
+
+LogIn.defaultProps = {
+
+}
+
+
+export default LogIn;
+          
