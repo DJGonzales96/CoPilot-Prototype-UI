8cfc0278718cc39a9db0e8d458ba2f0b0dc30701
Devin Gonzales <devin.j.gonzales@gmail.com>
2020-05-12 15:10:24 -0700

    init hazardInputForm page

---
+++ hazardInputForm/hazardInputForm.jsx
@@ -0,0 +1,63 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./hazardInputForm.css";
+
+class HazardInputForm extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="d1aad53c-af2d-48df-9f50-e2ac4ade7bc2" className="hazardInputForm">        <div data-layer="5d82c7ae-658c-479b-ac03-30a2bb4f47d0" className="navigationBar">            <svg data-layer="a2647dfb-89a8-4064-ac4c-e40480c8911f" preserveAspectRatio="none" viewBox="4907.00048828125 1088.9998779296875 375 67.999755859375" className="mergedBar"><path d="M 4907.00048828125 1156.999633789063 L 4907.00048828125 1108.999389648438 L 5282.00048828125 1108.999389648438 L 5282.00048828125 1156.999633789063 L 4907.00048828125 1156.999633789063 Z M 4907.00048828125 1108.999389648438 L 4907.00048828125 1088.999877929688 L 5282.00048828125 1088.999877929688 L 5282.00048828125 1108.999389648438 L 4907.00048828125 1108.999389648438 Z"  /></svg>
+            <div data-layer="581cdcb8-38aa-4b1e-84c0-9c84282f7628" className="tellUsMore">TELL US MORE</div>
+            <div data-layer="ccdc41e1-01a1-4627-97b4-8b4e3f377996" className="backwardArrow">                <svg data-layer="c487019f-c4f7-4146-a9b2-faaa6e19802f" preserveAspectRatio="none" viewBox="0 0 16 16" className="path10"><path d="M 8 0 L 6.545454978942871 1.454545497894287 L 12.05194854736328 6.961039066314697 L 0 6.961039066314697 L 0 9.038961410522461 L 12.05194854736328 9.038961410522461 L 6.545454978942871 14.54545497894287 L 8 16 L 16 8 L 8 0 Z"  /></svg>
+</div>
+</div>
+        <div data-layer="e8407952-f57f-4884-8b78-a08ea8988838" className="input1f324df8">            <div data-layer="909d2ecd-1a1a-4c51-8632-f4e00c53a46a" className="inputc97a169f">                <div data-layer="32ac6f40-4227-4434-9fba-2b9ee891b123" className="rectangle1408"></div>
+                <div data-layer="b2428852-e70b-4848-9b83-c98fa3098a9c" className="type">Type </div>
+</div>
+            <div data-layer="2fbc35be-3d9a-4978-95dd-50aab3e8a050" className="input446cd58f">                <div data-layer="de8e6be6-4415-418f-a55c-cc26f44cd7e8" className="rectangle1409"></div>
+                <div data-layer="e4d8029c-7963-4bcc-823d-c96f69c6d1d8" className="altitude">Altitude</div>
+</div>
+            <div data-layer="11893826-188e-4f9d-ba5b-891709c5849e" className="inputef78c5eb">                <div data-layer="d64c30da-a13e-4524-9f7b-314b3bc089ba" className="rectangle1410c3c66e4c"></div>
+                <div data-layer="c4876d15-1e65-442b-b878-91454ec799a5" className="radius">Radius</div>
+</div>
+            <div data-layer="38406baa-c4a9-4c20-8be8-0123f25f2d98" className="inputb9b79fc3">                <div data-layer="a0ba0f73-fd10-466a-9466-8acacff84ae3" className="rectangle14101a1c8496"></div>
+                <div data-layer="0efaf113-58ed-48c4-a0ff-1684c6201188" className="location">Location</div>
+</div>
+            <div data-layer="546897a2-9127-4851-bf40-af92770f67c7" className="input">                <div data-layer="c4734936-b339-4e06-81b4-85d26a7bdfa8" className="rectangle1410"></div>
+                <div data-layer="0d49f5de-9b7c-49a9-8b99-1b2f6981c83d" className="duration">Duration</div>
+</div>
+</div>
+        <div data-layer="36ce0e1a-e594-433a-945c-fa85053d49c0" className="textboxEmpty">            <div data-layer="6922a833-b3f2-4026-8456-38656825d93d" className="rectangle683"></div>
+            <div data-layer="d89307b3-fab3-41fa-928e-f45fff0987d9" className="typeSomething">Type something…</div>
+</div>
+        <div data-layer="658b53ea-96b1-4269-912c-6ab115d61a46" className="typeABriefDescription">Type a brief description ...</div>
+        <div data-layer="aef1da92-9952-4d1a-8cc5-07ac9c7aa4c9" className="continue">            <div data-layer="8fc36935-9707-495c-bd89-43d984a1ff77" className="rectangle119"></div>
+            <div data-layer="d40793e1-199d-4023-84e8-b4ae096f3d28" className="submitReport">SUBMIT REPORT</div>
+</div>
+</div>
+
+    );
+  }
+}
+
+HazardInputForm.propTypes = {
+
+}
+
+HazardInputForm.defaultProps = {
+
+}
+
+
+export default HazardInputForm;
+          
